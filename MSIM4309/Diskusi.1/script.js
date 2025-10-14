0a7d0a530d1e9a9fcaa936ccc50b923cebca7f99
rjhaikal <majidqoba52@gmail.com>
2025-10-14 15:47:44 +0700

    initial commit

---
+++ MSIM4309/Diskusi.1/script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('changeText');
+const text = document.getElementById('text');
+
+button.addEventListener('click', () => {
+  text.textContent = 'Teks ini telah diubah menggunakan JavaScript!';
+  alert('Teks berhasil diubah 🎉');
+});
